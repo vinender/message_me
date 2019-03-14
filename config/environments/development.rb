690a265496e3bfe34a7e7d99673b8dcf8d7c7ea9
Vinender Singh <vinendersingh91@gmail.com>
2019-03-14 16:08:10 +0530

    Update development.rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  # config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.allowed_request_origins = ['https://mashrur-messageme_test.herokuapp.com']
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = ['https://vinender-message-me.herokuapp.com']
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
 end
